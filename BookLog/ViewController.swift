144631905faaf75c54965f1844fdb979f7e70f45
JISUNG PARK <meowbutlerdev@gmail.com>
2025-02-14 16:13:13 +0900

    Initial Commit

---
+++ BookLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookLog
+//
+//  Created by 박지성 on 2/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookLogTests/BookLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookLogTests.swift
+//  BookLogTests
+//
+//  Created by 박지성 on 2/14/25.
+//
+
+import Testing
+@testable import BookLog
+
+struct BookLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
