c9c07bb11f434d9a91e94c597d591d760c560bd3
JISUNG PARK <meowbutlerdev@gmail.com>
2025-02-15 17:04:24 +0900

    feat: 책 추가 구현

---
+++ BookLog/ViewModel/BookListViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  BookListViewModel.swift
+//  BookLog
+//
+//  Created by 박지성 on 2/15/25.
+//
+
+import Foundation
+import CoreData
+
+final class BookListViewModel: NSObject, NSFetchedResultsControllerDelegate {
+    private let context = CoreDataManager.shared.context
+    private var fetchedResultsController: NSFetchedResultsController<Book>!
+
+    @Published var books: [Book] = []
+
+    override init() {
+        super.init()
+        setupFetchedResultsController()
+    }
+
+    private func setupFetchedResultsController() {
+        let fetchRequest: NSFetchRequest<Book> = Book.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: false)]
+
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                              managedObjectContext: context,
+                                                              sectionNameKeyPath: nil,
+                                                              cacheName: nil)
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+            books = fetchedResultsController.fetchedObjects ?? []
+        } catch {
+            print("Fetch Failed: \(error)")
+        }
+    }
+
+    func addBook(title: String, author: String, thumbnail: String?) {
+        let newBook = Book(context: context)
+        newBook.id = UUID()
+        newBook.title = title
+        newBook.author = author
+        newBook.thumbnail = thumbnail
+        newBook.createdAt = Date()
+
+        saveContext()
+    }
+
+    private func saveContext() {
+        do {
+            try context.save()
+        } catch {
+            print("Failed to save context: \(error)")
+        }
+    }
+}
